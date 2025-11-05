357bcfe62c992388ba1ffcc6d9f15a0670732a58
Muhammad Asad Azam <asadmughal88@gmail.com>
2025-11-05 20:13:12 +0500

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ Use absolute base for Netlify
+  base: './', // ✅ Use absolute base for Netlify
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
