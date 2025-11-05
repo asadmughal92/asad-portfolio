4c02cce06d9577d64790fee44291b2ea259b926d
Muhammad Asad Azam <asadmughal88@gmail.com>
2025-11-05 19:42:24 +0500

    fx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈 ADD THIS LINE
+  base: '/', // ✅ For Netlify, use '/' instead of './'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
