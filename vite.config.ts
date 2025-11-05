83e955414b55270f89d5478ff46eb65933ebce84
Muhammad Asad Azam <asadmughal88@gmail.com>
2025-11-05 19:48:27 +0500

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ For Netlify, use '/' instead of './'
+  base: './', // ✅ For Netlify, use '/' instead of './'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
